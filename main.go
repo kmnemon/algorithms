37f8b487e4ce24a8b86793927b737c6b95261985
kmnemon <kmnemon@outlook.com>
2024-04-10 13:52:19 +0800

    add algorithm introduce

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+O(log n) - log time, eg: binary search
+O(n) - linear time, eg: simple search
+O(n * log n) - fast sorting algorithm, eg: quicksort
+O(n^2) - a slow sorting algorithm, eg: selection sort
+O(n!) - a really slow algorithm, eg: traveling salesperson
+*/
+
+func main() {
+
+}
